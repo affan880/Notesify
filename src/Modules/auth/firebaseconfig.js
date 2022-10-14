43f95dd51e18166ed5eba32dc79f3221fceb4489
Syed Affan <60839175+affan880@users.noreply.github.com>
2022-10-14 18:44:59 +0530

    Update firebaseconfig.js

--- src/Modules/auth/firebaseconfig.js
+++ src/Modules/auth/firebaseconfig.js
@@ -1,9 +1,3 @@
 export default {
-  apiKey: "AIzaSyAWpt_Ifn4ZK9kRSDqWcoJfq9hUGPB-oj8",
-  authDomain: "notesify-6fbc3.firebaseapp.com",
-  projectId: "notesify-6fbc3",
-  storageBucket: "notesify-6fbc3.appspot.com",
-  messagingSenderId: "623837975426",
-  appId: "1:623837975426:web:aa3d84436bfaf3e7e3359b",
-  measurementId: "G-NN4D45C2X2"
+  
 };
